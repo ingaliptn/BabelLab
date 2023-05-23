c2b43ad516e517abb5318695d62c0e871794387d
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-05-24 01:08:06 +0300

    Практика 3. Сертифікації та стандарти

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import StudentInfo from './StudentInfo';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Мій проект React</h1>
+      <StudentInfo />
+    </div>
+  );
+};
+
+export default App;
